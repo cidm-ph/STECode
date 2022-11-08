b6911809eb92aebb6ee213e386752db11138c73a
LilWinx <lil.winx@gmail.com>
2022-11-08 16:14:53 +1100

    start calling parsers from gen_output

--- stecode/gen_output.py
+++ stecode/gen_output.py
@@ -7,6 +7,13 @@ stecvir_test = "/Users/winx/Documents/Bioinformatics/stecode/tests/data/test1_st
 recAstxeaehly_test = "/Users/winx/Documents/Bioinformatics/stecode/tests/data/test1_2recAstxeaehly.txt"
 
 import parsers.samtools_parse as sp
+import parsers.recAstxeaehly_parse as rp
+import parsers.stecvir_parse as vp
 
-def merge_all_NNs():
-    sp.samtools_input(samtools_test)
+def merge_all_NNs(stfile, recAfile, virfile, longread):
+    #NN1 = sp.samtools_input(stfile)
+    NN2 = rp.recA_input(recAfile, longread)
+    #NN3456 = vp.vfdb_input(virfile)
+
+    return NN2
+print(merge_all_NNs(samtools_test, recAstxeaehly_test, stecvir_test, False))
