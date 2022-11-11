eb5a0f3141393309ae5c7ccef03ab0c33018e8ee
LilWinx <lil.winx@gmail.com>
2022-11-11 15:32:38 +1100

    change cmds and add function strings

--- vicost/assists.py
+++ vicost/assists.py
@@ -3,19 +3,25 @@ import subprocess
 import sys
 import logging
 
-def execute_command(command, check=True):
+def run_cmd(command):
+    """
+    Run commands with error outputs.
+    """
     logging.debug('command: %s', command)
     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     result.stdout = result.stdout.decode()
     result.stderr = result.stderr.decode()
-    if check and result.returncode != 0:
+    if result.returncode != 0:
         logging.critical('Failed to run command: %s', result.args)
         logging.critical('stdout: %s', result.stdout)
         logging.critical('stderr: %s', result.stderr)
         sys.exit(1)
-    return 
+    return result
 
 def check_files(file):
+    """
+    Check input files if they exist and have contents
+    """
     if os.path.isfile(file) == True and os.stat(file).st_size != 0:
         truemsg = file + " exists and not empty, continuing..."
         logging.info(truemsg)
@@ -25,6 +31,9 @@ def check_files(file):
         sys.exit(1)
 
 def check_folders(folder):
+    """
+    Check the output folder if it exists, if not make new directory.
+    """
     if os.path.exists(folder) == True:
         truemsg = folder + " output folder exists"
         logging.info(truemsg)
