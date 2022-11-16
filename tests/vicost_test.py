ba8212746538de008fe8beac568d7659a1b17813
LilWinx <lil.winx@gmail.com>
2022-11-16 15:58:06 +1100

    update tests

--- tests/vicost_test.py
+++ tests/vicost_test.py
@@ -5,15 +5,22 @@ from .utils import cmd_runner
 
 vicost_cmd = cmd_runner(["python", "vicost"])
 
-@pytest.mar.parametrize(
+@pytest.mark.parametrize(
     "options,expected",
     [
         (["--name"], "argument --name/-n: expected one argument"),
-        ([], "the following arguments are required: --name/-n, --input/-i"),
-        (["--name", "test1"], "the following arguments are required: --input/-i"),
+        ([], "the following arguments are required: --R1, --R2, --name/-n"),
+        (["--name", "test1"], "the following arguments are required: --R1, --R2"),
         (["--name", "test1", "--outdir", 'outdir'],
-    "the following arguments are required: --input/-i"),
-])
+        "the following arguments are required: --R1, --R2"),
+        (["--R1", "test1", "--R2", "test"], "the following arguments are required: --name/-n")
+    ])
+
+def test_missing_args(options, expected):
+    out, err, code = vicost_cmd(options)
+    assert code == 2
+    assert expected in err
+    assert out == ''
 
 def test_cmd(out_dir):
     """
@@ -22,8 +29,10 @@ def test_cmd(out_dir):
     now = datetime.datetime.now()
     date = now.strftime("%Y%m%d")
     data_dir = "tests/data/"
+    r1 = os.path.join(data_dir, "test1_trimmed.paired_R1.fq.gz")
+    r2 = os.path.join(data_dir, "test1_trimmed.paired_R2.fq.gz")
 
-    out, err, code = vicost_cmd(["--R1", "--R1", "--name", "test1", "--outdir", out_dir, "--input", data_dir])
+    out, err, code = vicost_cmd(["--R1", r1, "--R2", r2, "--name", "test1", "--outdir", out_dir])
 
     assert code == 0
 
@@ -31,7 +40,7 @@ def test_cmd(out_dir):
         "test1_2recAstxeaehly.txt",
         "test1_eaesubtype.tab",
         "test1_stecvir.tab",
-        "test1_virbarcode %s" % (date),
+        "test1_virbarcode_%s.tab" % (date),
     ]
 
     for file in expected_files:
