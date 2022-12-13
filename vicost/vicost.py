104e02fa9f464118ad36321a2e10bb4afac2d663
LilWinx <lil.winx@gmail.com>
2022-12-13 14:34:00 +1100

    fix way outdir is selected when not supplied

--- vicost/vicost.py
+++ vicost/vicost.py
@@ -36,13 +36,23 @@ def vicost():
         version="vicoSt v%s" % __version__,
     )
     args = vars(parser.parse_args())
+    is_assembly = bool(args["fasta"] is not None)
+    is_reads = bool(args['R1'] is not None)
 
-    if args['R1'] is not None:
+    # cmd checks
+    if is_reads is True:
+        if args['R2'] is None:
+            logging.error("R2 was not provided, please provide the paired reads")
+            sys.exit(1)
+
+    if args['outdir'] is None and args['R1'] is not None:
         default = os.path.dirname(args['R1'])
-    if args["fasta"] is not None:
-        default = os.path.dirname(args['fasta'])
-    if args["outdir"] is None:
         outdir = default
+    elif args['outdir'] is None and args['fasta'] is not None:
+        default = os.path.dirname(args['fasta'])
+        outdir = default
+    else:
+        outdir = args["outdir"]
 
     logging.info(
         "Launching vicoSt v%s on %s and writing output files to directory %s",
@@ -57,15 +67,7 @@ def vicost():
         assists.check_abricate()
 
     ref = os.path.join(os.path.dirname(__file__), "database/stx_recA_eae.fasta")
-    is_assembly = bool(args["fasta"] is not None)
-    is_reads = bool(args['R1'] is not None)
-    
-    # cmd checks
-    if is_reads is True:
-        if args['R2'] is None:
-            logging.error("R2 was not provided, please provide the paired reads")
-            sys.exit(1)
-    
+   
     # checking file integrity and existence of output directory
     if all(item is not None for item in [args['fasta'], args['R1'], args['R2']]):
         assists.check_files(args["R1"])
@@ -74,8 +76,8 @@ def vicost():
         logging.info("Found fasta, R1 and R2, skipping Skesa")
 
         # skip skesa
-        cmd_runners.run_bwa(args["R1"], args["R2"], ref, args["name"], args["outdir"])
-        cmd_runners.run_solo_abricate("eaesub", "stecfinder", args["name"], args['fasta'], args["outdir"])
+        cmd_runners.run_bwa(args["R1"], args["R2"], ref, args["name"], outdir)
+        cmd_runners.run_solo_abricate("eaesub", "stecfinder", args["name"], args['fasta'], outdir)
 
     elif is_assembly is True and is_reads is False:
         assists.check_files(args["fasta"])
@@ -85,20 +87,20 @@ def vicost():
             logging.info("Running only Abricate on already assembled genomes, iso_tox will be DG")
 
         # run only abricate
-        cmd_runners.run_solo_abricate("eaesub", "stecfinder", args["name"], args['fasta'], args["outdir"])
+        cmd_runners.run_solo_abricate("eaesub", "stecfinder", args["name"], args['fasta'], outdir)
     else:
         assists.check_files(args["R1"])
         assists.check_files(args["R2"])
-        assists.check_folders(args["outdir"])
+        assists.check_folders(outdir)
 
         # Run bwa, samtools, skesa and abricate
-        cmd_runners.run_bwa(args["R1"], args["R2"], ref, args["name"], args["outdir"])
-        cmd_runners.run_skesa(args["R1"], args["R2"], args["name"], args["outdir"])
-        cmd_runners.run_abricate("eaesub", "stecfinder", args["name"], args["outdir"])
+        cmd_runners.run_bwa(args["R1"], args["R2"], ref, args["name"], outdir)
+        cmd_runners.run_skesa(args["R1"], args["R2"], args["name"], outdir)
+        cmd_runners.run_abricate("eaesub", "stecfinder", args["name"], outdir)
 
     # vicost portion - file check
-    file1 = os.path.join(args["outdir"], args["name"] + "_eaesubtype.tab")
-    file3 = os.path.join(args["outdir"], args["name"] + "_sfindAbricate.tab")
+    file1 = os.path.join(outdir, args["name"] + "_eaesubtype.tab")
+    file3 = os.path.join(outdir, args["name"] + "_sfindAbricate.tab")
 
     assists.check_files(file1)
     assists.check_files(file3)
@@ -106,17 +108,17 @@ def vicost():
     if is_assembly is True and is_reads is False:
         file2 = "skip"
     else:
-        file2 = os.path.join(args["outdir"], args["name"] + "_2recAstxeae.txt")
+        file2 = os.path.join(outdir, args["name"] + "_2recAstxeae.txt")
         assists.check_files(file2)
 
     # run vicost
     go_df = go.merge_all_NNs(
         file1, file2, file3, is_reads, args["name"], args["longread"]
     )
-    go.gen_output(args["name"], args["outdir"], go_df)
+    go.gen_output(args["name"], outdir, go_df)
     logging.info(
         "Complete :D please check %s for the STEC barcode for your sample",
-        args["outdir"],
+        outdir,
     )
 
 
