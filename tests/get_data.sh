4427eea155af47de80b71b08c94650643a6b86e3
Carl Suster <Carl.Suster@health.nsw.gov.au>
2022-12-14 15:18:18 +1100

    Document get_tests

--- tests/get_data.sh
+++ tests/get_data.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+# Downloads data needed for tests
+#
+# NB: currently requires access to a specific server
+
 set -e
 
 DATA_FILES=(
-  "tests/data/test1_trimmed.paired_R1.fq.gz"
-  "tests/data/test1_trimmed.paired_R2.fq.gz"
-  "tests/data/test2_trimmed.paired_R1.fq.gz"
-  "tests/data/test2_trimmed.paired_R2.fq.gz"
+  "test1_trimmed.paired_R1.fq.gz"
+  "test1_trimmed.paired_R2.fq.gz"
+  "test2_trimmed.paired_R1.fq.gz"
+  "test2_trimmed.paired_R2.fq.gz"
 )
 
 get_file() {
   rsync "hpc.sydney.edu.au:/project/pgraw/stec/fastq/$1" "tests/data/$1"
 }
 
+if ! test -d "tests/data"; then
+  "ERROR: run from main STECode directory" >&2
+  exit 1
+fi
+
 for file in "${DATA_FILES[@]}"; do
-  if ! test -e "$file"; then
-    name="$(basename "$file")"
-    echo "FETCHING: $name"
-    get_file "$name"
+  if test -e "tests/data/$file"; then
+    echo "OK:  $file"
+  else
+    echo "GET: $file"
+    get_file "$file"
   fi
 done
