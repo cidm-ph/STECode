23d454de31873ac3d6fa5277fe8420680d9f9356
Carl Suster <Carl.Suster@health.nsw.gov.au>
2022-12-14 15:02:47 +1100

    Remove test data files

--- tests/get_data.sh
+++ tests/get_data.sh
@@ -9,8 +9,14 @@ DATA_FILES=(
   "tests/data/test2_trimmed.paired_R2.fq.gz"
 )
 
+get_file() {
+  rsync "hpc.sydney.edu.au:/project/pgraw/stec/fastq/$1" "tests/data/$1"
+}
+
 for file in "${DATA_FILES[@]}"; do
   if ! test -e "$file"; then
-    echo "MISSING: $file"
+    name="$(basename "$file")"
+    echo "FETCHING: $name"
+    get_file "$name"
   fi
 done
