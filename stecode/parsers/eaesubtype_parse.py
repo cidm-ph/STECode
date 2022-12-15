25ad9d5bf70d1c077f3ae51999c2ddd7a444327b
LilWinx <lil.winx@gmail.com>
2022-12-15 15:18:47 +1100

    change df.append() to pd.concat

--- stecode/parsers/eaesubtype_parse.py
+++ stecode/parsers/eaesubtype_parse.py
@@ -3,6 +3,7 @@ Getting the necessary subtype from abricate data
 """
 
 import pandas as pd
+import numpy as np
 import logging
 
 
@@ -16,9 +17,8 @@ def eaesubtype_input(file, name):
     if eaesubtype_df.empty:
         msg = "This sample does not contain eae"
         logging.info(msg)
-        eaesubtype_df = eaesubtype_df.append(
-            pd.Series("00", index=eaesubtype_df.columns), ignore_index=True
-        )
+        null_row = pd.DataFrame("00", index=np.arange(1), columns=eaesubtype_df.columns)
+        eaesubtype_df = pd.concat([eaesubtype_df, null_row])
     eaesubtype_df["#Sequence_ID"] = name
     eaesubtype_df["eae_sub"] = eaesubtype_df["GENE"].str[:2]
     return eaesubtype_df
