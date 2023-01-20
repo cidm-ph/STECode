change to multimap to ensure correct normalisation

stecode/parsers/recAstxeae_parse.py
@@ -8,10 +8,22 @@ import logging
 import sys
 
 heirarchy = ["12", "02", "01", "T2", "T1", "00"]
+col_names = [
+    "#rname",
+    "startpos",
+    "endpos",
+    "numreads",
+    "covbases",
+    "coverage",
+    "meandepth",
+    "meanbaseq",
+    "meanmapq",
+]
 
 
 def recA_input(file):
-    stx_df = pd.read_csv(file, sep="\t", header=0)
+    stx_df = pd.read_csv(file, sep="\t", names=col_names)
+    stx_df.to_csv(file, sep="\t", index=False)
     if stx_df.empty:
         msg = "This sample did not contain any stx genes, please check again if this sample is STEC. Exiting"
         logging.error(msg)
@@ -20,7 +32,11 @@ def recA_input(file):
         msg = "The sample in question did not successfully detect the recA gene in samtools, retry or check manually"
         logging.error(msg)
         sys.exit(1)
-    re_stx_df = stx_df[stx_df["coverage"] >= 95]
+    re_stx_df = stx_df[stx_df["coverage"] >= 98]
+    if re_stx_df.empty:
+        msg = "stx genes not detected in sequencing reads. Exiting"
+        logging.error(msg)
+        sys.exit(1)
     re_stx_df["virgene"] = (
         re_stx_df["#rname"].str.split("_").str.get(-1)
     )  # change this to last not second last.
@@ -30,20 +46,20 @@ def recA_input(file):
     ].values[0]
     filt_stx_df["Normalised"] = filt_stx_df["meandepth"] / div
     conditions = [
-        (filt_stx_df["Normalised"] > 2.2)
+        (filt_stx_df["Normalised"] > 2.1)
         & (filt_stx_df["virgene"].str.contains("stx1").any())
-        & (filt_stx_df["virgene"].str.contains("stx2").any()),
-        (filt_stx_df["Normalised"] > 2.2)
-        & (filt_stx_df["virgene"].str.contains("stx2").any()),
-        (filt_stx_df["Normalised"] > 2.2)
-        & (filt_stx_df["virgene"].str.contains("stx1").any()),
-        (filt_stx_df["Normalised"] <= 2.2)
+        & (filt_stx_df["virgene"].str.contains("stx2").any()),  # Triggers 12
+        (filt_stx_df["Normalised"] > 2.1)
+        & (filt_stx_df["virgene"].str.contains("stx2").any()),  # Triggers 02
+        (filt_stx_df["Normalised"] > 2.1)
+        & (filt_stx_df["virgene"].str.contains("stx1").any()),  # Triggers 01
+        (filt_stx_df["Normalised"] <= 2.1)
         & (filt_stx_df["Normalised"] >= 2)
-        & (filt_stx_df["virgene"].str.contains("stx2").any()),
-        (filt_stx_df["Normalised"] <= 2.2)
+        & (filt_stx_df["virgene"].str.contains("stx2").any()),  # Triggers T2
+        (filt_stx_df["Normalised"] <= 2.1)
         & (filt_stx_df["Normalised"] >= 2)
-        & (filt_stx_df["virgene"].str.contains("stx1").any()),
-        (filt_stx_df["Normalised"] < 2),
+        & (filt_stx_df["virgene"].str.contains("stx1").any()),  # Triggers T1
+        (filt_stx_df["Normalised"] < 2),  # Triggers 00
     ]
     filt_stx_df["iso_tox"] = np.select(conditions, heirarchy, default="XX")
     sub_stx_df = filt_stx_df.drop_duplicates(subset=["virgene"])