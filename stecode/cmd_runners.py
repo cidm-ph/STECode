c7081b4db231f4cc89d7f3920b1f3bd41a5e72d9
LilWinx <lil.winx@gmail.com>
2023-01-23 11:58:24 +1100

    remove sed for parallel function

--- stecode/cmd_runners.py
+++ stecode/cmd_runners.py
@@ -10,9 +10,11 @@ def run_bwa(fq1, fq2, ref, name, outdir):
     Run BWA and Samtools to generate the 2recAstxeae text file.
     """
     refname = ref.split("_")[2][:-6]
-    command1 = f"bwa mem -t 8 {ref} {fq1} {fq2} | samtools view --threads 8 -b -S | samtools sort --threads 8 -o {outdir}/{name}_{refname}.sorted.bam"
-    command2 = f"samtools index -b {outdir}/{name}_{refname}.sorted.bam"
-    command3 = f"samtools coverage {outdir}/{name}_{refname}.sorted.bam > {outdir}/{name}_{refname}.txt && sed -n '2p' {outdir}/{name}_{refname}.txt >> {outdir}/{name}_2recAstxeae.txt"
+    bamdir = outdir + "/" + name + "/bams"
+    stuffdir = outdir + "/" + name
+    command1 = f"bwa mem -t 8 {ref} {fq1} {fq2} | samtools view --threads 8 -b -S | samtools sort --threads 8 -o {bamdir}/{name}_{refname}.sorted.bam"
+    command2 = f"samtools index -b {bamdir}/{name}_{refname}.sorted.bam"
+    command3 = f"samtools coverage {bamdir}/{name}_{refname}.sorted.bam > {bamdir}/{name}_{refname}.txt"
 
     assists.run_cmd(command1)
     assists.run_cmd(command2)
@@ -23,7 +25,8 @@ def run_skesa(fq1, fq2, name, outdir):
     """
     Run Skesa for input into Abricate.
     """
-    command4 = f"skesa --fastq {fq1},{fq2} --contigs_out {outdir}/{name}.contigs.fa"
+    stuffdir = outdir + "/" + name
+    command4 = f"skesa --fastq {fq1},{fq2} --contigs_out {stuffdir}/{name}.contigs.fa"
     assists.run_cmd(command4)
 
 
@@ -31,9 +34,9 @@ def run_abricate(eaesub_db, stecvir_db, name, outdir):
     """
     Run Abricate to get the eaesubtype and stecvirulence
     """
-
-    command5 = f"abricate --datadir {assists.stecode_db_dir} --db {eaesub_db} --minid 90.0 --mincov 90.0 {outdir}/{name}.contigs.fa > {outdir}/{name}_eaesubtype.tab"
-    command6 = f"abricate --datadir {assists.stecode_db_dir} --mincov 21 --db {stecvir_db} {outdir}/{name}.contigs.fa > {outdir}/{name}_sfindAbricate.tab"
+    stuffdir = outdir + "/" + name
+    command5 = f"abricate --datadir {assists.stecode_db_dir} --db {eaesub_db} --minid 90.0 --mincov 90.0 {outdir}/{name}.contigs.fa > {stuffdir}/{name}_eaesubtype.tab"
+    command6 = f"abricate --datadir {assists.stecode_db_dir} --mincov 21 --db {stecvir_db} {stuffdir}/{name}.contigs.fa > {stuffdir}/{name}_sfindAbricate.tab"
     assists.run_cmd(command5)
     assists.run_cmd(command6)
 
@@ -42,8 +45,8 @@ def run_solo_abricate(eaesub_db, stecvir_db, name, infile, outdir):
     """
     Run Abricate to get the eaesubtype and stecvirulence for FASTAs only
     """
-
-    command5 = f"abricate --datadir {assists.stecode_db_dir} --db {eaesub_db} --minid 90.0 --mincov 90.0 {infile} > {outdir}/{name}_eaesubtype.tab"
-    command6 = f"abricate --datadir {assists.stecode_db_dir} --mincov 21 --db {stecvir_db} {infile} > {outdir}/{name}_sfindAbricate.tab"
+    stuffdir = outdir + "/" + name
+    command5 = f"abricate --datadir {assists.stecode_db_dir} --db {eaesub_db} --minid 90.0 --mincov 90.0 {infile} > {stuffdir}/{name}_eaesubtype.tab"
+    command6 = f"abricate --datadir {assists.stecode_db_dir} --mincov 21 --db {stecvir_db} {infile} > {stuffdir}/{name}_sfindAbricate.tab"
     assists.run_cmd(command5)
     assists.run_cmd(command6)
